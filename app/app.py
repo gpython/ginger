220d4d2d56cd436025c94078800df1befb5ab170
weining.zhu <zhuwn@juzhongjoy.com>
2018-07-12 16:26:47 +0800

    Ginger

---
+++ app/app.py
@@ -0,0 +1,13 @@
+#encoding:utf-8
+from flask import Flask
+
+
+def create_app():
+  app = Flask(__name__)
+
+  app.config.from_object('app.config.secure')
+  app.config.from_object('app.config.setting')
+
+  return app
+
+  pass
